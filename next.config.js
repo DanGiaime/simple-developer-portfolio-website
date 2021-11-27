31d53d4d3eeac6c75a217a84274803409a5dab97
Rox Works <dsg6874@g.rit.edu>
2021-11-27 16:00:46 +0700

    fix /stream

--- next.config.js
+++ next.config.js
@@ -1,54 +1,49 @@
 const withTM = require('next-transpile-modules')(['react-markdown']);
 
 module.exports = withTM({
-    webpack5: false
-    // async redirects() {
-    //   return [
-    //     {
-    //         source: '/stream',
-    //         destination: 'https://twitch.tv/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/twitch',
-    //         destination: 'https://twitch.tv/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/twitter',
-    //         destination: 'https://twitter.com/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/instagram',
-    //         destination: 'https://instagram.com/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/ig',
-    //         destination: 'https://instagram.com/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/youtube',
-    //         destination: 'https://youtube.com/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/yt',
-    //         destination: 'https://youtube.com/roxkstar74',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/future',
-    //         destination: 'https://www.youtube.com/watch?v=CIsrhoVSc4',
-    //         permanent: true,
-    //     },
-    //     {
-    //         source: '/leaving',
-    //         destination: 'https://youtu.be/xP3m27Pl5eU',
-    //         permanent: true,
-    //     }
-    //   ]
-    // },
+    webpack5: false,
+    async redirects() {
+      return [
+        {
+            source: '/twitch',
+            destination: 'https://twitch.tv/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/twitter',
+            destination: 'https://twitter.com/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/instagram',
+            destination: 'https://instagram.com/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/ig',
+            destination: 'https://instagram.com/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/youtube',
+            destination: 'https://youtube.com/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/yt',
+            destination: 'https://youtube.com/roxkstar74',
+            permanent: true,
+        },
+        {
+            source: '/future',
+            destination: 'https://www.youtube.com/watch?v=CIsrhoVSc4',
+            permanent: true,
+        },
+        {
+            source: '/leaving',
+            destination: 'https://youtu.be/xP3m27Pl5eU',
+            permanent: true,
+        }
+      ]
+    },
   })
---
+++ pages/stream.js
@@ -0,0 +1,20 @@
+export default function Stream() {
+    return (<>
+        <head>
+            <meta name='twitter:card' content='player' />
+            <meta name='twitter:site' content='@roxkstar74' />
+            <meta name='twitter:player' content="https://player.twitch.tv/?channel=roxkstar74&parent=twitter.com&parent=www.twitter.com&parent=rox.works&parent=www.rox.works&parent=cards-dev.twitter.com&parent=cards-frame.twitter.com&parent=tweetdeck.twitter.com&parent=mobile.twitter.com&autplay=true" />
+            <meta name='twitter:player:width' content='1280' />
+            <meta name='twitter:player:height' content='720' />
+            <meta name='twitter:player:stream' content='true' />
+            <meta name='twitter:player:stream:type' content='live' />
+            <meta name='twitter:description' content="Building my startup TwitchToTikTok live from Thailand! Come hang :D" />
+            <meta name='twitter:image' content='https://github.com/roxkstar74.png' />
+            <meta name='twitter:title' content='Cowork with me live!' />
+            <meta http-equiv="Refresh" content="0; url='https://twitch.tv/roxkstar74'" />
+        </head>
+        <body>
+            spaghetti
+        </body>
+    </>)
+}
