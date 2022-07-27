246506b1cbf9c16339ef197ff1b4d26644cce71b
Rox Works <dsg6874@g.rit.edu>
2022-07-27 01:41:07 +0100

    hmmm

---
+++ pages/beast.js
@@ -0,0 +1,12 @@
+export default function beast() {
+    return (<div>
+        <embed type="text/html" width="100%"
+            src="https://mixerno.space/embed/youtube-channel-counter/UCX6OQ3DkcsbYNE6H8uQQuVA" />
+        <embed type="text/html" width="100%"
+            src="https://mixerno.space/embed/youtube-channel-counter/UCUaT_39o1x6qWjz7K2pWcgw" />
+        <embed type="text/html" width="100%"
+            src="https://mixerno.space/embed/youtube-channel-counter/UCIPPMRA040LQr5QPyJEbmXA" />
+        <embed type="text/html" width="100%"
+            src="https://mixerno.space/embed/youtube-channel-counter/UCZzvDDvaYti8Dd8bLEiSoyQ" />
+    </div>);
+}
